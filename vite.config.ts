cbc4dc6fffa7a3bb55aadcc93c0994d0adaef001
Gaurav Singh <kumarharendra6668@gmail.com>
2026-04-25 22:23:43 +0530

    Clean up vite.config.ts by removing exports
    Remove unnecessary export and configuration for Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -21,10 +21,5 @@ export default defineConfig(({mode}) => {
       hmr: process.env.DISABLE_HMR !== 'true',
     },
   };
-});  
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: "./"
-})
+});
 
